1e4344c6ef9b050c0b1e9070b87fdc14fcf1f8e2
Lathifnurirsyad <lathifni.ni@gmail.com>
2022-12-08 00:44:15 +0700

    notif nih, hamdallah

---
+++ app/src/main/java/id/ac/unand/e_kp/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,60 @@
+package id.ac.unand.e_kp.firebase
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import id.ac.unand.e_kp.R
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    private val channel_id = "contoh"
+    private val notifId = 1
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("token", "tokennya adalah $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val a = message.notification?.title.toString()
+        val b = message.notification?.body.toString()
+
+        createNotificationChannel()
+
+        Log.d("message", "message $a dan bodynya $b")
+        showNotification(a, b)
+    }
+
+    fun showNotification (title: String, message: String){
+        val builder = NotificationCompat.Builder(this, channel_id)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_mhs_selesai_kp)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+//       val manager = NotificationManagerCompat.from(this)
+//       manager.notify(222, builder.build())
+        with(NotificationManagerCompat.from(this)){
+            notify(1, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification title"
+            val descriptionText = "Notification description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channel_id, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+}
